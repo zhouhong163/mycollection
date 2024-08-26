c203bdc7d8edc1b034119ad7c4ca9a15df46ffbe
zhouhong1632003 <345724624@qq.com>
2024-08-26 09:33:40 +0800

    scond commit

--- src/com/heping/gitTest/GitTest.java
+++ src/com/heping/gitTest/GitTest.java
@@ -3,5 +3,6 @@ package com.heping.gitTest;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
