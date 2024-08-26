00bbf05b4644276d58ef1508c96c6b5d664aacaf
zhouhong163 <43284988+zhouhong163@users.noreply.github.com>
2024-08-26 15:12:29 +0800

    pull test commit

--- src/com/heping/gitTest/GitTest.java
+++ src/com/heping/gitTest/GitTest.java
@@ -10,5 +10,7 @@ public class GitTest {
         System.out.println("hot-fix conflict test");
         System.out.println("push test");
         System.out.println("push test2");
+        System.out.println("pull test2");
+        
     }
 }
